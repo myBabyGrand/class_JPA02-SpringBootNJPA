53a5c0a4af7af1b974969efb2ea5545e0ea24884
mybabygrand <amuro_ray@kakao.com>
2021-04-06 23:26:24 +0900

    chapter2 Entity developing.

---
+++ src/main/java/jpabook/jpashop/domain/Delivery.java
@@ -0,0 +1,19 @@
+package jpabook.jpashop.domain;
+
+import javax.persistence.*;
+
+public class Delivery {
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; //[READY, COMPLETED]
+}
