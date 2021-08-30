bafd2895600c82209ca7bae67c3bfbafad37dc1a
mybabygrand <amuro_ray@kakao.com>
2021-08-30 22:24:44 +0900

    Order search V2

--- src/main/java/jpabook/jpashop/api/OrderSimpleAPIController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleAPIController.java
@@ -1,13 +1,18 @@
 package jpabook.jpashop.api;
 
+import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
 import jpabook.jpashop.domain.OrderSearch;
+import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -26,5 +31,29 @@ public class OrderSimpleAPIController {
         return all;
 
     }
+    @GetMapping("/api/v2/simple-orders")
+    public List<SimpleOrderDto> ordersV2(){
+        List<SimpleOrderDto> result = orderRepository.findAllByJPQL(new OrderSearch()).stream()
+                .map(SimpleOrderDto::new)
+                .collect(Collectors.toList());
+        return result;
+    }
 
+    @Data
+    static class SimpleOrderDto{
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+
+        public SimpleOrderDto(Order order){
+            orderId = order.getId();
+            name = order.getMember().getName();//Lazy 초기화
+            orderDate = order.getOrderDate();
+            orderStatus= order.getStatus();
+            address = order.getDelivery().getAddress();//Lazy 초기화
+        }
+
+    }
 }
