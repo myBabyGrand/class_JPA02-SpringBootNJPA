e94b1f0b73b90e3588a8b52a195536c580282888
mybabygrand <amuro_ray@kakao.com>
2021-09-09 22:23:37 +0900

    Section 4-6 orderV5

--- src/main/java/jpabook/jpashop/api/OrderApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -91,5 +91,9 @@ public class OrderApiController {
     public List<OrderQueryDto> orderV4(){
         return orderQueryRepository.findOrderQueryDtos();
     }
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> orderV5(){
+        return orderQueryRepository.findAllByDto_optimization();
+    }
 
 }
